0128febf2d74d4724d907038705842785c1873fb
Augustine <austynomilano77@gmail.com>
2023-11-29 11:59:52 -0800

    simple match updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,4 +2,24 @@
 # This script accepts one argument and passes it to a regular expression,
 # matching method
 # The regular expression must match School
-puts ARGV[0].scan(/School/).join
+
+# script.rb
+pattern = /School/
+
+# Check if an argument is provided
+if ARGV.length != 1
+  puts "Usage: ruby script.rb <input_string>"
+  exit
+end
+
+input_string = ARGV[0]
+
+# Perform the regular expression match
+matches = input_string.scan(pattern)
+
+# Output the result
+if matches.empty?
+  puts "No match found."
+else
+  puts "Matches found: #{matches.join(', ')}"
+end
